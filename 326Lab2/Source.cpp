4c775dcd45c8eec20737295b60694fc92cfc3736
Brian LaBarbera <labarberabrian00@gmail.com>
2023-02-19 14:37:50 -0500

    3/3 done

--- 326Lab2/Source.cpp
+++ 326Lab2/Source.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-
 float computeEx(float x, int n, int &count);
+void insertionSort(int* ptr, int n);
+void printArray(int* ptr, int n);
 void TowerofHanoi(char source, char destination, char spare, int n);
 void Problem1();
+void Problem2();
 void Problem3();
 
 int main() {
 	
-	Problem3();
+	Problem2();
 
 	return 0;
 }
@@ -26,7 +28,28 @@ void Problem1() {
 	std::cout << x << " raised to the power of " << n << " is " << computeEx(x, n, count) << std::endl;
 }
 
+void Problem2() {
+	
+	int n = 0;
+
+	std::cout << "Enter number of integers to be sorted:" << std::endl;
+	std::cin >> n;
+
+	int* ptr = new int[n];
+
+	std::cout << "Enter numbers:" << std::endl;
+	
+	for (int i = 0; i < n; i++) {
+		std::cin >> ptr[i];
+	}
+
+	insertionSort(ptr, n);
+
+	printArray(ptr, n);
+}
+
 void Problem3() {
+	
 	char source = 'A';
 	char destination = 'B';
 	char spare = 'C';
@@ -54,6 +77,39 @@ float computeEx(float x, int n, int &count) {
 	else {
 		return x * computeEx(x, n - 1, count);
 	}
+}
+
+void insertionSort(int* ptr, int n) {
+
+	if (n <= 1) {
+		return;
+	}
+
+	else {
+		insertionSort(ptr, n - 1);
+
+		int end = ptr[n - 1];
+		int j = n - 2;
+
+		while (j >= 0 && ptr[j] > end) {
+
+			ptr[j + 1] = ptr[j];
+			j--;
+
+		}
+
+		ptr[j + 1] = end;
+
+	}
+
+}
+
+void printArray(int* ptr, int n) {
+
+	for (int i = 0; i < n; i++) {
+		std::cout << ptr[i] << " ";
+	}
+
 }
 
 void TowerofHanoi(char source, char destination, char spare, int n) {
